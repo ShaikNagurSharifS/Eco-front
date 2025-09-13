2da5b2a7d01b1d643ca7f70f64fadcebfedcf3ef
ShaikNagurSharifS <ShaikNagurSharif@outlook.com>
2025-09-13 18:55:13 +0530

    Changes in fronend by tailwind addition

--- Eco/src/App.tsx
+++ Eco/src/App.tsx
@@ -7,7 +7,8 @@ function App() {
   return (
     <>
       <div>
-        <h1> This is Eco Project </h1>
+        <h1 className="text-3xl text-cyan-400 font-bold italic underline"> This is Eco Project </h1>
+        <h1> Welcome </h1>
       </div>
     </>
   )
--- Eco/vite.config.ts
+++ Eco/vite.config.ts
@@ -1,10 +1,15 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+
+import { defineConfig, type PluginOption } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
+
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()] as PluginOption[],
   server: {
     port: 3001 // Replace 3001 with your desired port number
   },
 })
+
+
